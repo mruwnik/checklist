83eff5bc70854a0c875acb9328679da4951a57a9
Daniel O'Connell <tojad99@gmail.com>
2019-02-03 18:30:24 +0100

    Use new cider commands to start REPL

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,5 @@
-((nil . ((cider-refresh-before-fn . "reloaded.repl/suspend")
-         (cider-refresh-after-fn  . "reloaded.repl/resume")
-         (cider-cljs-lein-repl    . "(do (user/go) (user/cljs-repl))"))))
+((nil
+  (eval . (cider-register-cljs-repl-type 'chestnut "(do (user/go) (user/cljs-repl))"))
+  (cider-refresh-before-fn . "reloaded.repl/suspend")
+  (cider-refresh-after-fn  . "reloaded.repl/resume")
+  (cider-default-cljs-repl . chestnut)))
